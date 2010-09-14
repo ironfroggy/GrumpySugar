5452f9fb8187e2db16221679ec6ecabfaf4c3844
Calvin Spealman <ironfroggy@gmail.com>
2010-09-14 18:52:12 -0400

    Implement basic movement of sprites within a level.

--- grumpysugar.base.js
+++ grumpysugar.base.js
@@ -28,16 +28,93 @@ $GS.Room = function Room(options) {
         .addGroup('gs-actor', group_options).end()
         ;
 };
-$.extend($GS.Room.prototype, {
-     addSprite: function(options) {
+(function(){
+    var sprite_counter = 0;
+    function get_sprite_id() {
+        var id = sprite_counter;
+        sprite_counter++;
+        return 'gs-sprite-' + id;
+    }
+
+    $.extend($GS.Room.prototype, {
+         addSprite: function(options) {
+            return new Sprite(options);
+         }
+    });
+
+    $GS.Sprite = function Sprite(options) {
+        this.element_id = get_sprite_id();
         $('#gs-' + options.group || 'things')
-            .addSprite(options.name, {animation: options.animation,
+            .addSprite(this.element_id, {animation: options.animation,
                 width: 32, height: 32});
-     }
-    ,
+        this.element = $('#' + this.element_id);
+        this.x = 0;
+        this.y = 0;
+    };
+
+    var directions_to_offset = {
+        l: {x: -1}
+        ,r: {x: 1}
+        ,u: {y: -1}
+        ,d: {y: 1}
+    };
+    $.extend($GS.Sprite.prototype, {
+         step: function(direction, done) {
+            this._stop();
+            var x, y;
+            x = directions_to_offset[direction].x || 0;
+            y = directions_to_offset[direction].y || 0;
+
+            this.x += x;
+            this.y += y;
+
+            this._update_position(done);
+        }
+        ,position: function() {
+            return {x: this.x, y: this.y};
+        }
+        ,walkTo: function(x, y) {
+            var self = this;
+            var tries = 10;
+            function oneStep() {
+                if (tries < 0) {
+                    return;
+                } else {
+                    tries--;
+                }
+
+                if (x > self.x) {
+                    self.step('r', oneStep);
+                } else if (x < self.x) {
+                    self.step('l', oneStep);
+                } else if (y > self.y) {
+                    self.step('d', oneStep);
+                } else if (y < self.y) {
+                    self.step('u', oneStep);
+                }
+            }
+            oneStep();
+        }
+        ,_stop: function() {
+            this.element.clearQueue();
+        }
+        ,_update_position: function(done) {
+            this.element.animate({
+                 top: 32 * this.y
+                ,left: 32 * this.x
+            }, 500, 'linear', (done||function(){}));
+        }
+    });
+
+})();
+
+$.each("Room Sprite".split(' '), function(i, name) {
+    if (typeof window[name] === "undefined") {
+        window[name] = $GS[name];
+    }
 });
 
-
+var sprite;
 $(document).ready(function(){
     var playground = $('#playground').playground({
         height: $GS.GAME_HEIGHT, width: $GS.GAME_WIDTH
@@ -51,7 +128,7 @@ $(document).ready(function(){
         ,height_tiles: 10
     });
 
-    test_room.addSprite({
+    sprite = test_room.addSprite({
          animation: new $.gameQuery.Animation({imageURL: "hero.png"})
         ,name: 'test_sprite'
         ,group: 'actor'
