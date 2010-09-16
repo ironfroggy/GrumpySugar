9d1d975509bed68bb4fc9f271db5f56fdb832e70
Calvin Spealman <ironfroggy@gmail.com>
2010-09-16 17:03:21 -0400

    Add revese paths from the second to start rooms.

--- grumpysugar.test_map.js
+++ grumpysugar.test_map.js
@@ -9,5 +9,11 @@ TEST_MAP = {
     ,'second': {
         width: 15
         ,height: 4
+        ,triggers: {
+            '0:0': 'start:3:0',
+            '0:1': 'start:3:1',
+            '0:2': 'start:3:2',
+            '0:3': 'start:3:3',
+        }
     }
 }
